2cf338e05b101b42c2bde231c71c2632fc4ea1e7
Doug Bell <doug@preaction.me>
2021-05-31 19:13:32 -0500

    add basic application example

---
+++ application_test.go
@@ -0,0 +1,28 @@
+package mojo_test
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/preaction/mojo.go"
+)
+
+func ExampleApplicationHelloWorld() {
+	app := mojo.Application{}
+	app.Routes.Get("/").To(func(*mojo.Controller) { fmt.Print("Hello, World!") })
+
+	// XXX: Create mojo.URL wrapper
+	// Everything needs a damned wrapper to be even remotely usable...
+	url, _ := url.ParseRequestURI("/")
+	c := mojo.Controller{
+		Req: &mojo.Request{Method: "GET", URL: url},
+		Stash: map[string]interface{}{
+			"path": url.Path,
+		},
+	}
+
+	app.Routes.Dispatch(&c)
+
+	// Output:
+	// Hello, World!
+}
