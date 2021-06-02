29a8d4a3b3aedf58bb6bbe302ad427640150e502
Doug Bell <doug@preaction.me>
2021-06-02 12:04:05 -0500

    add some basic package/app documentation

--- application.go
+++ application.go
@@ -1,21 +1,37 @@
 package mojo
 
-// Application is the main type for an application to use.
+// Application is the main type for an application to use. Applications
+// register route handlers and hook handlers, and contain all the
+// global application configuration and tools that can be used by those
+// handlers.
 type Application struct {
 	Routes Routes
 	Hooks  map[Hook][]HookHandler
 }
 
+// Hook is label for an application event that can have HookHandlers
+// assigned to it. These handlers can modify the Context to modify the
+// Request or Response.
 type Hook string
+
+// HookHandler is a function that is registered to be called for a given
+// Hook
 type HookHandler func(*Context)
 
 const (
+	// BeforeDispatch is a hook that is called before the
+	// Application.Dispatch function is called for the current Context
 	BeforeDispatch Hook = "BeforeDispatch"
-	AfterDispatch  Hook = "AfterDispatch"
+	// AfterDispatch is a hook that is called after the
+	// Application.Dispatch function is called for the current Context
+	AfterDispatch Hook = "AfterDispatch"
 )
 
 // XXX: Use go embed to embed templates and static files
 
+// BuildContext fills in the context from its Request object, including
+// setting the default stash values from the Application and any stash
+// values that come from the Request
 func (app *Application) BuildContext(c *Context) *Context {
 	c.Stash = map[string]interface{}{}
 	// XXX: Add defaults from application
@@ -26,6 +42,7 @@ func (app *Application) BuildContext(c *Context) *Context {
 	return c
 }
 
+// Hook registers a Hook handler.
 func (app *Application) Hook(hook Hook, handler HookHandler) {
 	if app.Hooks == nil {
 		app.Hooks = map[Hook][]HookHandler{}
@@ -37,7 +54,9 @@ func (app *Application) Hook(hook Hook, handler HookHandler) {
 	}
 }
 
-func (app *Application) Emit(hook Hook, c *Context) {
+// emit emits a hook event with the given context
+// XXX: We may need different arguments for future hooks
+func (app *Application) emit(hook Hook, c *Context) {
 	hooks, ok := app.Hooks[hook]
 	if !ok {
 		return
@@ -47,11 +66,14 @@ func (app *Application) Emit(hook Hook, c *Context) {
 	}
 }
 
+// Handler handles the request for the given Context. This includes
+// dispatching the BeforeDispatch and AfterDispatch hooks and writing
+// the response to the user (if it hasn't been already)
 func (app *Application) Handler(c *Context) {
 	// Look up route and call handler
-	app.Emit(BeforeDispatch, c)
+	app.emit(BeforeDispatch, c)
 	app.Routes.Dispatch(c)
-	app.Emit(AfterDispatch, c)
+	app.emit(AfterDispatch, c)
 
 	// Write the response
 	if c.Res.Code == 0 {
---
+++ doc.go
@@ -0,0 +1,7 @@
+// package mojo is a web application framework, ported from the
+// Mojolicious web framework for the Perl language.
+//
+// In the Mojolicious framework, an author builds an Application object,
+// registers handlers for one or more Routes, and then starts the
+// application using its (TODO) Start() method.
+package mojo
