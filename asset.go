d51773b13000af789dcc14e917e2ea942e0ba572
Doug Bell <doug@preaction.me>
2021-07-03 14:18:29 -0500

    add asset types for better handling of content

---
+++ asset.go
@@ -0,0 +1,87 @@
+package mojo
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+// Asset is a container for a Message's contents
+type Asset interface {
+	Length() int64
+	Serve(http.ResponseWriter) error
+	ServeRange(w http.ResponseWriter, start int64, end int64) error
+}
+
+// NewAsset builds an Asset from the given content, which can be a File
+// object, a string, or an array of bytes.
+func NewAsset(content interface{}) Asset {
+	switch v := content.(type) {
+	case File:
+		return FileAsset{path: v}
+	case *os.File:
+		return FileAsset{path: NewFile(v.Name())}
+	case string:
+		return MemoryAsset{buffer: []byte(v)}
+	case []byte:
+		return MemoryAsset{buffer: v}
+	}
+	panic(fmt.Sprintf("mojo.NewAsset: Unknown type %t", content))
+}
+
+// FileAsset is an Asset backed by a file on a filesystem
+type FileAsset struct {
+	path File
+}
+
+// Length returns the length of the file
+func (asset FileAsset) Length() int64 {
+	return asset.path.Stat().Size()
+}
+
+// Serve writes the entire file contents to the given http.ResponseWriter
+func (asset FileAsset) Serve(w http.ResponseWriter) error {
+	// XXX: Go makes us copy/paste because receiver can't be interface?
+	// XXX: Does calculating the length slow this down?
+	return asset.ServeRange(w, 0, asset.Length()-1)
+}
+
+// ServeRange writes the given range to the given http.ResponseWriter.
+// start and end are byte positions of the first and last byte to send,
+// respectively.
+func (asset FileAsset) ServeRange(w http.ResponseWriter, start int64, end int64) error {
+	file := asset.path.Open()
+	if _, err := file.Seek(start, io.SeekStart); err != nil {
+		panic(fmt.Sprintf("Could not Seek to %d: %v", start, err))
+	}
+	// XXX: Check that all requested was written?
+	_, _ = io.CopyN(w, file, 1+end-start)
+	return nil
+}
+
+// MemoryAsset is an Asset stored in memory
+type MemoryAsset struct {
+	buffer []byte
+}
+
+// Length returns the length of the buffer
+func (asset MemoryAsset) Length() int64 {
+	return int64(len(asset.buffer))
+}
+
+// Serve writes the entire buffer contents to the given http.ResponseWriter
+func (asset MemoryAsset) Serve(w http.ResponseWriter) error {
+	// XXX: Go makes us copy/paste because receiver can't be interface?
+	// XXX: Does calculating the length slow this down?
+	return asset.ServeRange(w, 0, asset.Length()-1)
+}
+
+// ServeRange writes the given range to the given http.ResponseWriter.
+// start and end are byte positions of the first and last byte to send,
+// respectively.
+func (asset MemoryAsset) ServeRange(w http.ResponseWriter, start int64, end int64) error {
+	_, err := w.Write(asset.buffer[start : end+1])
+	// XXX: Check that all requested was written?
+	return err
+}
---
+++ asset_test.go
@@ -0,0 +1,98 @@
+package mojo_test
+
+import (
+	"bytes"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/preaction/mojo.go"
+)
+
+func TestNewAsset(t *testing.T) {
+	asset := mojo.NewAsset([]byte("foobar"))
+	if _, ok := asset.(mojo.MemoryAsset); !ok {
+		t.Errorf("NewAsset([]byte) did not return MemoryAsset. Got: %t", asset)
+	}
+
+	asset = mojo.NewAsset("foobar")
+	if _, ok := asset.(mojo.MemoryAsset); !ok {
+		t.Errorf("NewAsset(string) did not return MemoryAsset. Got: %t", asset)
+	}
+
+	file, err := os.CreateTemp("", "*")
+	if err != nil {
+		t.Fatalf("Could not create temp file: %v", err)
+	}
+	defer os.RemoveAll(file.Name())
+	asset = mojo.NewAsset(file)
+	if _, ok := asset.(mojo.FileAsset); !ok {
+		t.Errorf("NewAsset(os.File) did not return FileAsset. Got: %t", asset)
+	}
+
+	path := mojo.NewFile(file.Name())
+	asset = mojo.NewAsset(path)
+	if _, ok := asset.(mojo.FileAsset); !ok {
+		t.Errorf("NewAsset(mojo.File) did not return FileAsset. Got: %t", asset)
+	}
+}
+
+func TestMemoryAsset(t *testing.T) {
+	buf := []byte(`{"foo":"bar","fizz":"buzz"}`)
+	asset := mojo.NewAsset(buf)
+
+	if asset.Length() != int64(len(buf)) {
+		t.Errorf(`MemoryAsset.Length() incorrect. Got: %d, Expect: %d`, asset.Length(), len(buf))
+	}
+
+	w := httptest.NewRecorder()
+	err := asset.Serve(w)
+	if err != nil {
+		t.Errorf(`MemoryAsset.Serve() returned error: %v`, err)
+	}
+	resBody := w.Body.Bytes()
+	if !bytes.Equal(resBody, buf) {
+		t.Errorf(`MemoryAsset.Serve() body incorrect. Got: %s, Expect %s`, resBody, buf)
+	}
+
+	w = httptest.NewRecorder()
+	err = asset.ServeRange(w, 1, 5)
+	if err != nil {
+		t.Errorf(`MemoryAsset.ServeRange() returned error: %v`, err)
+	}
+	resBody = w.Body.Bytes()
+	if !bytes.Equal(resBody, []byte(`"foo"`)) {
+		t.Errorf(`MemoryAsset.ServeRange() body incorrect. Got: %s, Expect %s`, resBody, []byte(`"foo"`))
+	}
+}
+
+func TestFileAsset(t *testing.T) {
+	buf := []byte(`{"foo":"bar"}`)
+	temp := mojo.TempFile()
+	temp.Spurt(buf)
+	asset := mojo.NewAsset(temp)
+
+	if asset.Length() != int64(len(buf)) {
+		t.Errorf(`FileAsset.Length() incorrect. Got: %d, Expect: %d`, asset.Length(), len(buf))
+	}
+
+	w := httptest.NewRecorder()
+	err := asset.Serve(w)
+	if err != nil {
+		t.Errorf(`FileAsset.Serve() returned error: %v`, err)
+	}
+	resBody := w.Body.Bytes()
+	if !bytes.Equal(resBody, buf) {
+		t.Errorf(`FileAsset.Serve() body incorrect. Got: %s, Expect %s`, resBody, buf)
+	}
+
+	w = httptest.NewRecorder()
+	err = asset.ServeRange(w, 1, 5)
+	if err != nil {
+		t.Errorf(`FileAsset.ServeRange() returned error: %v`, err)
+	}
+	resBody = w.Body.Bytes()
+	if !bytes.Equal(resBody, []byte(`"foo"`)) {
+		t.Errorf(`FileAsset.ServeRange() body incorrect. Got: %s, Expect %s`, resBody, []byte(`"foo"`))
+	}
+}
